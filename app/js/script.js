6d854b18719671045ff92c16de43d0b20dfebcdc
jeremyling <jling825@gmail.com>
2021-02-09 19:18:14 -0800

    Part 2: Animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function() {
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')) { // Close Haumburger Menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else { // Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+
+    }
+});
